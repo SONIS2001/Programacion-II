deb979c5c9068b92e08751693113a5a2bd03ade4
SONIS2001 <32625095+SONIS2001@users.noreply.github.com>
2017-10-20 21:01:42 -0300

    ejercicio3

--- src/ejercicios/TrabajoPractico1.java
+++ src/ejercicios/TrabajoPractico1.java
@@ -6,70 +6,100 @@ import java.util.List;
 
 /**
  * Arreglos y Listas
+ *
  * @author arielp
  */
 public class TrabajoPractico1 {
-    
+
+    private int[] arrayNumeros;
+
     public void ejercicio1() {
         String[] palabras = new String[4];
         palabras[0] = "Esto";
         palabras[1] = "es";
         palabras[2] = "una";
         palabras[3] = "frase";
-        
+
         System.out.println("Iterando Arreglos forma 1");
         for (int i = 0; i < palabras.length; i++) {
             String palabra = palabras[i];
             System.out.println(palabra);
         }
         System.out.println("\n");
-        
+
         System.out.println("Iterando Arreglos forma 2");
         for (String palabra : palabras) {
             System.out.println(palabra);
         }
     }
-    
+
     public void ejercicio2() {
         List<String> palabras = new ArrayList();
         palabras.add("Esto");
         palabras.add("es");
         palabras.add("una");
         palabras.add("frase");
-        
+
         System.out.println("Iterando Listas forma 1");
         for (int i = 0; i < palabras.size(); i++) {
             String palabra = palabras.get(i);
             System.out.println(palabra);
         }
         System.out.println("\n");
-        
+
         System.out.println("Iterando Listas forma 2");
         for (String palabra : palabras) {
             System.out.println(palabra);
         }
-        
+
         System.out.println("Iterando Listas forma 3 (Iterador)");
         Iterator<String> iterador = palabras.iterator();
-        while(iterador.hasNext()) {
+        while (iterador.hasNext()) {
             System.out.println(iterador.next());
         }
     }
-    
+
     /**
-     * Crear un arreglo de numeros enteros del 1 al 5, recorrerlos y mostralos de las 2 formas
+     * Crear un arreglo de numeros enteros del 1 al 5, recorrerlos y mostralos
+     * de las 2 formas
      */
     public void ejercicio3() {
-        // aca hicimos un cambio
-        
-        
-        // mas cambios
+        int[] arrayNumeros = new int[5];
+        arrayNumeros[0] = 1;
+        arrayNumeros[1] = 2;
+        arrayNumeros[2] = 3;
+        arrayNumeros[3] = 4;
+        arrayNumeros[4] = 5;
+
+        for (int i = 0; i <= 4; i++) {
+
+            System.out.println(arrayNumeros[i]);
+        }
+
     }
-    
+
     /**
-     * Crear una lista de numeros enteros del 1 al 5, recorrerlos y mostarlos de las 3 formas
+     * Crear una lista de numeros enteros del 1 al 5, recorrerlos y mostarlos de
+     * las 3 formas
      */
     public void ejercicio4() {
-        
+
+    }
+
+    /*
+
+    
+    /**
+     * @return the arrayNumeros
+     */
+    public int[] getArrayNumeros() {
+        return arrayNumeros;
+    }
+
+    /**
+     * @param arrayNumeros the arrayNumeros to set
+     */
+    public void setArrayNumeros(int[] arrayNumeros) {
+        this.arrayNumeros = arrayNumeros;
     }
 }
