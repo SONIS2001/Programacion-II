cba794fa236600f6ef5602980cdea920f26de25a
Sonia Trejo <32625095+SONIS2001@users.noreply.github.com>
2017-11-02 23:35:57 -0300

    Add files via upload

---
+++ TrabajoPractico1.java
@@ -0,0 +1,136 @@
+package ejercicios;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Arreglos y Listas
+ *
+ * @author arielp
+ */
+public class TrabajoPractico1 {
+
+    private int[] arrayNumeros;
+
+    public void ejercicio1() {
+        String[] palabras = new String[4];
+        palabras[0] = "Esto";
+        palabras[1] = "es";
+        palabras[2] = "una";
+        palabras[3] = "frase";
+
+        System.out.println("Iterando Arreglos forma 1");
+        for (int i = 0; i < palabras.length; i++) {
+            String palabra = palabras[i];
+            System.out.println(palabra);
+        }
+        System.out.println("\n");
+
+        System.out.println("Iterando Arreglos forma 2");
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+    }
+
+    public void ejercicio2() {
+        List<String> palabras = new ArrayList();
+        palabras.add("Esto");
+        palabras.add("es");
+        palabras.add("una");
+        palabras.add("frase");
+
+        System.out.println("Iterando Listas forma 1");
+        for (int i = 0; i < palabras.size(); i++) {
+            String palabra = palabras.get(i);
+            System.out.println(palabra);
+        }
+        System.out.println("\n");
+
+        System.out.println("Iterando Listas forma 2");
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+
+        System.out.println("Iterando Listas forma 3 (Iterador)");
+        Iterator<String> iterador = palabras.iterator();
+        while (iterador.hasNext()) {
+            System.out.println(iterador.next());
+        }
+
+    }
+
+    /**
+     * Crear un arreglo de numeros enteros del 1 al 5, recorrerlos y mostralos
+     * de las 2 formas
+     */
+    public void ejercicio3() {
+        int[] arrayNumeros = new int[5];
+        arrayNumeros[0] = 1;
+        arrayNumeros[1] = 2;
+        arrayNumeros[2] = 3;
+        arrayNumeros[3] = 4;
+        arrayNumeros[4] = 5;
+        System.out.println("Iterando Arreglos forma 1");
+
+        for (int i = 0; i <= 4; i++) {
+
+            System.out.println(arrayNumeros[i]);
+        }
+
+        System.out.println("Iterando Arreglos forma 2");
+        for (int arrayNum : arrayNumeros) {
+            System.out.println(arrayNum);
+
+        }
+        /**
+         * Crear una lista de numeros enteros del 1 al 5, recorrerlos y
+         * mostarlos de las 3 formas
+         */
+    }
+
+    public void ejercicio4() {
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+
+        System.out.println("Iterando Listas forma 1");
+        for (int i = 0; i < numeros.size(); i++) {
+            int numero = numeros.get(i);
+            System.out.println(numeros);
+        }
+        System.out.println("\n");
+
+        System.out.println("Iterando Listas forma 2");
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+
+        System.out.println("Iterando Listas forma 3 (Iterador)");
+        Iterator<Integer> iterador = numeros.iterator();
+        while (iterador.hasNext()) {
+            System.out.println(iterador.next());
+        }
+    }
+
+    /*
+
+    
+    /**
+     * @return the arrayNumeros
+     */
+    public int[] getArrayNumeros() {
+        return arrayNumeros;
+    }
+
+    /**
+     * @param arrayNumeros the arrayNumeros to set
+     */
+    public void setArrayNumeros(int[] arrayNumeros) {
+        this.arrayNumeros = arrayNumeros;
+    }
+}
---
+++ TrabajoPractico2.java
@@ -0,0 +1,81 @@
+package ejercicios;
+
+import excepciones.ExcepcionSimpatica;
+import excepciones.MiExcepcion;
+
+/**
+ * Excepciones
+ * @author arielp
+ */
+public class TrabajoPractico2 {
+
+    public void ejercicio1() {
+        int[] array = new int[20];
+        array[-7] = 24;
+        System.out.println("No tengo que llegar hasta aca");
+    }
+
+    public void ejercicio2() {
+        try {
+            int[] array = new int[20];
+            array[50] = 24;
+            System.out.println("no debo llegar aca");
+            
+            // Instrucciones cuando no hay una excepción
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            // Instrucciones cuando se produce una excepcion
+            System.out.println("Excepcion: " + ex);     
+        } finally {
+            System.out.println("Finally Ejercicio 2");
+            // Instruciones que se ejecutan, tanto si hay como sino hay excepciones
+        }
+    }
+
+    public void ejercicio3() {
+        int[] array = new int[20];
+        try {
+            int b = 0;
+            int a = 23 / b;
+            array[-10] = 24;            
+
+            String s = null;
+            s.equals("QQQQ");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println(" Error de índice en un array: " + ex);
+        } catch (ArithmeticException ex) {
+            System.out.println(" Error matematico: " + ex);
+        } catch (Exception ex) {
+            System.out.println(" Error matematico: " + ex);
+        } 
+    }
+    
+    public void ejercicio4() throws Exception {
+        System.out.println("Generando una excepcion");
+        throw new Exception("Soy la excepcion");
+    }
+    
+    /**
+     * Excepcion Propia
+     */
+    public void ejercicio5() {
+        try {
+            System.out.println("Primera parte");
+            throw new ExcepcionSimpatica("Soy la excepcion");
+        }catch(ExcepcionSimpatica e) {
+            System.out.println("excepcion" +  e);
+        } 
+    }
+    
+    /**
+     * Crear una excepcion propia y capturarla y mostrar su contenido.
+     */
+    public void ejercicio6() {
+         try {
+            System.out.println("Saldra impreso primero");
+            throw new MiExcepcion("Soy la excepcion creada por Sonia");
+        }catch(MiExcepcion e) {
+            System.out.println("excepcion" +  e);
+        }
+    }
+}
+
